0eace6e03475a8921ec4787325858836884a211a
xWacob <89878961+xWacob@users.noreply.github.com>
2023-07-06 18:15:41 -0400

    Make game a 5 round game with winner/loser

--- RPS.js
+++ RPS.js
@@ -1,3 +1,34 @@
+// ***************** GLOBALS ******************************
+
+let userGuess;
+let cpuGuess;
+let cleanUserGuess;
+let outcome;
+
+// ********************************************************
+
+let userScore = 0;
+let cpuScore = 0;
+
+let gameCheck = game();
+
+if (gameCheck === 0)
+{
+    if(userScore > cpuScore)
+    {
+        console.log(`You beat the CPU by a score of ${userScore} to ${cpuScore}, good guessing!`);
+    }
+    else
+    {
+        console.log(`The CPU beat you by a score of ${cpuScore} to ${userScore}, better luck next time!`);
+    }
+}
+else
+{
+    console.log("ERROR OCCURRED");
+}
+
+
 function getComputerChoice()
 {
     let cpuChoice = Math.floor(Math.random() * 100);
@@ -19,7 +50,7 @@ function getComputerChoice()
     return strCPUGuess;
 }
 
-let outcome;
+
 function playOneGame(playerSelection, computerSelection)
 {
     
@@ -95,12 +126,33 @@ function playOneGame(playerSelection, computerSelection)
     return outcome;
 }
 
-let cpuGuess = getComputerChoice(); // gets number between 0 and 2, inclusive
+function game()
+{
+    let gameOutcome;
+    for(i = 0; i < 5; i++)
+    {
+        cpuGuess = getComputerChoice(); // gets number between 0 and 2, inclusive
 
-// console.log(cpuGuess);
+        userGuess = prompt("Please pick Rock, Paper, or Scissors: "); // ask user for their input
+        cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
 
-let userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
-let cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
+        gameOutcome = playOneGame(cleanUserGuess, cpuGuess); 
 
-let gameOutcome = playOneGame(cleanUserGuess, cpuGuess);
-console.log(gameOutcome);
+        if(gameOutcome === "CPU Win")
+        {
+            cpuScore++;
+        }
+        else if (gameOutcome === "User Win")
+        {
+            userScore++;
+        }
+        else
+        {
+            console.log("Invalid event occurred, exiting...");
+            return null;
+        }
+    }
+
+    // game runs correctly
+    return 0;
+}
