feb78ba8db40c0178fd26c72b43ba3d324ae800e
xWacob <89878961+xWacob@users.noreply.github.com>
2023-07-05 20:48:44 -0400

    Add logic for user choice other than rock
    Either choose paper, scissors, or an invalid item and if it is invalid,
    then it reasks

--- RPS.js
+++ RPS.js
@@ -23,21 +23,67 @@ function playOneGame(playerSelection, computerSelection)
 {
     if (playerSelection === "Rock")
     {
-        if(computerSelection === "Paper")
+        if(computerSelection === "Paper") // lose to cpu
         {
             console.log("Paper beats Rock! You Lose!");
         }
-        else if(computerSelection === "Scissors")
+        else if(computerSelection === "Scissors") // beat cpu
         {
             console.log("Congratulations! Rock beats Scissors! You Win!");
         }
-        else
+        else // tie, go again
         {
-            console.log("It's a tie, you both picked rock")
+            console.log("It's a tie, you both picked Rock. Pick again")
             userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
             cpuGuess = getComputerChoice();
-            playOneGame(userGuess, cpuGuess);
+            cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
+            playOneGame(cleanUserGuess, cpuGuess);
         }
+    }
+    else if (playerSelection === "Paper") // user picks paper
+    {
+        if(computerSelection === "Scissors") // lose to cpu
+        {
+            console.log("Scissors beats Paper! You Lose!");
+        }
+        else if(computerSelection === "Rock") // beat cpu
+        {
+            console.log("Congratulations! Paper beats Rock! You Win!");
+        }
+        else // tie, go again
+        {
+            console.log("It's a tie, you both picked Paper. Pick again")
+            userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
+            cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
+            cpuGuess = getComputerChoice();
+            playOneGame(cleanUserGuess, cpuGuess);
+        }
+    }
+    else if (playerSelection === "Scissors") // user picks scissors
+    {
+        if(computerSelection === "Rock") // lose to cpu
+        {
+            console.log("Rock beats Scissors! You Lose!");
+        }
+        else if(computerSelection === "Paper") // beat cpu
+        {
+            console.log("Congratulations! Scissors beats Paper! You Win!");
+        }
+        else // tie, go again
+        {
+            console.log("It's a tie, you both picked Scissors. Pick again")
+            userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
+            cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
+            cpuGuess = getComputerChoice();
+            playOneGame(cleanUserGuess, cpuGuess);
+        }
+    }
+    else
+    {
+        console.log("You chose something invalid!! Try again");
+        userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
+        cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
+        playOneGame(cleanUserGuess, cpuGuess);
     }
 }
 
@@ -46,6 +92,6 @@ let cpuGuess = getComputerChoice(); // gets number between 0 and 2, inclusive
 // console.log(cpuGuess);
 
 let userGuess = prompt("Please pick Rock, Paper, or Scissors: ");
-const cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1);
+let cleanUserGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1).toLowerCase();
 
-playOneGame(userGuess, cpuGuess);
+playOneGame(cleanUserGuess, cpuGuess);
